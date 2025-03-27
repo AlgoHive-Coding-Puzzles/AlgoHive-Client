6d7b9824f61a6dbf367efd0cc54f6d84bc9a5e9d
Eric-Philippe <ericphlpp@gmail.com>
2025-03-27 22:24:21 +0100

    feat: add login redirection to button in UsersListCompetitions component

--- src/components/UsersListCompetitions.tsx
+++ src/components/UsersListCompetitions.tsx
@@ -62,7 +62,12 @@ const UsersListCompetitions: React.FC<UsersListCompetitionsProps> = ({
                       {t("users.connectYourAccount")}
                     </h2>
 
-                    <button className="mt-6 bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-0.5 text-lg font-semibold leading-6 text-white inline-block">
+                    <button
+                      className="mt-6 bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-0.5 text-lg font-semibold leading-6 text-white inline-block"
+                      onClick={() => {
+                        window.location.href = "/login";
+                      }}
+                    >
                       <span className="absolute inset-0 overflow-hidden rounded-full">
                         <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
                       </span>
